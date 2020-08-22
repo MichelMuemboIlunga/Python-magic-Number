adcb59ddac84560fff49c4fdd0238567c8ce4c82
MichelMuemboIlunga <61247320+MichelMuemboIlunga@users.noreply.github.com>
2020-08-22 22:36:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,36 +4,40 @@ Cancel = "Cancel"
 
 for n in range(5):
     # prompting user to enter the magic number
-    user = int(input("Please Enter your Magic number: "))
-    # add user number to the list
-    MagicNumber.append(user)
+    try:
+        user = int(input("Please Enter your Magic number: "))
 
-    # user confirmation to continue or cancel the process
-    if True:
-        print("The value you enter was : ", user)
-        UserConfirm = input(" Enter Confirm to Continue or cancel to Cancel: ")
-        if UserConfirm == confirmation:
-            continue
-        elif UserConfirm == Cancel:
-            break
+        # add user number to the list
+        MagicNumber.append(user)
 
-        # in case the user enter a wrong confirmation keyword instead of confirm or Cancel
-        else:
-            print("------------------------------------------")
-            print("Not a correct input, Please try  again!!!")
-            print("------------------------------------------")
-            if True:
-                print("The Number you enter was : ", user)
-                UserConfirm = input(" Enter Confirm to Continue or cancel to Cancel: ")
-                if UserConfirm == confirmation:
-                    continue
-                elif UserConfirm == Cancel:
-                    break
+        # user confirmation to continue or cancel the process
+        if True:
+            print(f"The value you enter was : {user}")
+            UserConfirm = input(" Enter Confirm to Continue or cancel to Cancel: ")
+            if UserConfirm == confirmation:
+                continue
+            elif UserConfirm == Cancel:
+                break
 
+            # in case the user enter a wrong confirmation keyword instead of confirm or Cancel
+            else:
+                print("------------------------------------------")
+                print("Not a correct input, Please try  again!!!")
+                print("------------------------------------------")
+                if True:
+                    print("The Number you enter was : ", user)
+                    UserConfirm = input(" Enter Confirm to Continue or cancel to Cancel: ")
+                    if UserConfirm == confirmation:
+                        continue
+                    elif UserConfirm == Cancel:
+                        break
+    # if user enter a wrong type of number (ex:string or float) instead of throwing an error
+    # user receive a message
+    except ValueError:
+        print("wrong type of number, Prefer integer")
 
 print("Your Magic Number are: ", MagicNumber)
 
-
 if __name__ == '__main__':
+    print('All done by Michel Muembo Ilunga 💻')
 
-    print('All done')
